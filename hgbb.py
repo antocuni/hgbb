d4d34d74ad9ba3da1e10e70d5e199ef1ecd9aae8
Antonio Cuni <anto.cuni@gmail.com>
2011-04-01 10:47:32 +0200

    add a bblink command to get bitbucket links

--- hgbb.py
+++ hgbb.py
@@ -302,6 +302,22 @@ def bb_followers(ui, repo, **opts):
             follower['first_name'],
             follower['last_name']))
 
+def bb_link(ui, repo, filename=None, **opts):
+    '''display a bitbucket link to the repository, or the the specific file if given'''
+    # XXX: might not work on windows, because it uses \ to separate paths
+    reponame = get_bbreponame(ui, repo, opts)
+    nodeid = str(repo[None])
+    if nodeid.endswith('+'):
+        # our wc is dirty, just take the node id and be happy
+        nodeid = nodeid[:-1]
+    if filename:
+        path = os.path.relpath(filename, repo.root)
+    else:
+        path = ''
+    url = 'http://bitbucket.org/%s/src/%s/%s'
+    url = url % (reponame, nodeid, path)
+    ui.write(url + '\n')
+
 def clone(orig, ui, source, dest=None, **opts):
     if source[:2] == 'bb' and ':' in source:
         protocol, rest = source.split(':', 1)
@@ -346,6 +362,10 @@ cmdtable = {
            'name of the repo at bitbucket (else guessed from repo dir)'),
           ],
          'hg bbcreate [-d desc] [-l lang] [-w site] reponame'),
+    'bblink':
+        (bb_link,
+         [],
+         'hg bblink filename'),
 }
 
 commands.norepo += ' bbcreate'
